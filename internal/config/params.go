dd1d1b9c68689fafdd9a4e8e500305c79c35984e
nikitos <nikitachukov@gmail.com>
2025-10-01 04:04:13 +0300

    Add environment variable support for server address and base URL, with fallback to flags and defaults

--- internal/config/params.go
+++ internal/config/params.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"flag"
+	"os"
 )
 
 var (
@@ -10,7 +11,27 @@ var (
 )
 
 func ParseParams() {
-	AppAddr = flag.String("a", "localhost:8080", "адрес запуска HTTP-сервера")
-	BasePath = flag.String("b", "", "базовый адрес результирующего сокращённого URL")
+
+	pFlagAppAddr := flag.String("a", "localhost:8080", "адрес запуска HTTP-сервера")
+	pFlagBasePath := flag.String("b", "", "базовый адрес результирующего сокращённого URL")
 	flag.Parse()
+
+	AppAddr := os.Getenv("SERVER_ADDRESS")
+	if AppAddr == "" {
+		if *pFlagAppAddr != "" {
+			AppAddr = *pFlagAppAddr
+		}
+	}
+
+	BasePath := os.Getenv("BASE_URL")
+	if BasePath == "" {
+		if *pFlagBasePath != "" {
+			BasePath = *pFlagBasePath
+		}
+	}
+
+	//Если указана переменная окружения, то используется она.
+	//Если нет переменной окружения, но есть аргумент командной строки (флаг), то используется он.
+	//Если нет ни переменной окружения, ни флага, то используется значение по умолчанию.
+
 }
