2fb9e831be50c314099459e6f46a5687199130ac
nikitos <nikitachukov@gmail.com>
2025-10-27 13:57:08 +0300

    Implement URL persistence in `DbRepo`: add `FindShortURL`, `GetLongURL`, and `Set` methods with basic error handling and timeouts. Update test data.

--- internal/repository/dbrepo/repo.go
+++ internal/repository/dbrepo/repo.go
@@ -53,13 +53,37 @@ func (r *DBRepo) Save() {
 }
 
 func (r *DBRepo) FindShortURL(long string) (string, bool) {
-	return "", false
+	var short string
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	row := r.db.QueryRowContext(ctx, "select key from shortener where original_url=$1 limit 1", long)
+	err := row.Scan(&short)
+	if err != nil {
+		panic(err)
+	}
+	if short != "" {
+		return short, true
+	} else {
+		return "", false
+	}
 }
 
 func (r *DBRepo) GetLongURL(short string) (string, bool) {
-	return "", false
+	var long string
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	row := r.db.QueryRowContext(ctx, "select original_url from shortener where key=$1 limit 1", short)
+	err := row.Scan(&long)
+	if err != nil {
+		panic(err)
+	}
+	if short != "" {
+		return long, true
+	} else {
+		return "", false
+	}
 }
 
 func (r *DBRepo) Set(short, long string) {
-
+	_, err := r.db.Exec("INSERT INTO shortener (key, original_url) VALUES ($1, $2)", short, long)
+	if err != nil {
+	}
 }
