d007a3f3918d0ce214125a6ee0669a245814c59f
nikitos <nikitachukov@gmail.com>
2025-10-27 19:01:40 +0300

    Refactor `DbRepo` implementation: enhance DSN parsing, replace query context with error checks, add migration support, and update dependencies.

--- internal/repository/dbrepo/repo.go
+++ internal/repository/dbrepo/repo.go
@@ -3,17 +3,25 @@ package dbrepo
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
+	"log"
 	"sync"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	_ "github.com/jackc/pgx/v5/stdlib"
 	//"github.com/nikitachukov/url_shortener.git/internal/model"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/nikitachukov/url_shortener.git/internal/logger"
 )
 
 type DBRepo struct {
-	db *sql.DB
-	mu sync.RWMutex
+	db  *sql.DB
+	mu  sync.RWMutex
+	dns string
 }
 
 func (r *DBRepo) Ping() bool {
@@ -30,14 +38,29 @@ func (r *DBRepo) Ping() bool {
 }
 
 func NewDB(dsn string) *DBRepo {
+	cnf, err := pgx.ParseConfig(dsn)
+	if err != nil {
+		fmt.Println("Error parsing DSN:", err)
+	}
 
-	db, err := sql.Open("pgx", dsn)
+	mydsn := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=disable", cnf.User, cnf.Password, cnf.Host, cnf.Port, cnf.Database)
+
+	db, err := sql.Open("pgx", mydsn)
 	if err != nil {
 		panic(err)
 	}
 
+	m, err := migrate.New("file://migrations", mydsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := m.Up(); err != nil {
+
+	}
+
 	repo := &DBRepo{
-		db: db,
+		db:  db,
+		dns: mydsn,
 	}
 
 	logger.Log.Sugar().Infof("DB connection established")
@@ -54,36 +77,27 @@ func (r *DBRepo) Save() {
 
 func (r *DBRepo) FindShortURL(long string) (string, bool) {
 	var short string
-	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
-	row := r.db.QueryRowContext(ctx, "select key from shortener where original_url=$1 limit 1", long)
-	err := row.Scan(&short)
-	if err != nil {
-		panic(err)
-	}
-	if short != "" {
-		return short, true
-	} else {
+	if err := r.db.QueryRow("select key from shortener where original_url=$1 limit 1", long).Scan(&short); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", false
+		}
 		return "", false
 	}
+	return short, true
 }
 
 func (r *DBRepo) GetLongURL(short string) (string, bool) {
 	var long string
-	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
-	row := r.db.QueryRowContext(ctx, "select original_url from shortener where key=$1 limit 1", short)
-	err := row.Scan(&long)
-	if err != nil {
-		panic(err)
-	}
-	if short != "" {
-		return long, true
-	} else {
+	if err := r.db.QueryRow("select original_url from shortener where key=$1 limit 1", short).Scan(&long); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return "", false
+		}
 		return "", false
 	}
+	return long, true
+
 }
 
 func (r *DBRepo) Set(short, long string) {
-	_, err := r.db.Exec("INSERT INTO shortener (key, original_url) VALUES ($1, $2)", short, long)
-	if err != nil {
-	}
+	r.db.Exec("INSERT INTO shortener (key, original_url) VALUES ($1, $2)", short, long)
 }
